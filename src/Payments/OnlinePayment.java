8fbec678f621265b4dd1489cc470d1e67f6caf77
AbdulSamad4068 <53683979+AbdulSamad4068@users.noreply.github.com>
2020-03-25 23:29:47 +0500

    Updated OnlinePayment class
    hey buddy! just completed OnlinePayment class. So check it out.

--- src/Payments/OnlinePayment.java
+++ src/Payments/OnlinePayment.java
@@ -1,6 +1,37 @@
 package Payments;
 
+import java.time.LocalDateTime;
+
 import Payments.Payment;
 
+// Completed by Abdul Samad
 public class OnlinePayment extends Payment {
+	private long account_no;
+	private String email;
+	private String passwd;
+	
+	public OnlinePayment(String id, LocalDateTime date, double amount,long account_no,String email,String passwd){
+		super(id,date,amount);
+		this.account_no=account_no;
+		this.email=email;
+		this.passwd=passwd;
+	}
+	
+	public long getAccountNo() {
+		return account_no;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getPassword() {
+		return passwd;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("ID: %s\nDate: %s\nAmount: %.2f\nAccount No: %d\nEmail: %s\nPassword: %s", getId(),getDate(),getAmount(),getAccountNo(),getEmail(),getPassword());
+	}
 }
+
