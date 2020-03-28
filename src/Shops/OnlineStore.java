2e900f33140ea8cb8eeee773d769f8a0e27a7bb3
Abdul Samad <abdul.samad4068@gmail.com>
2020-03-28 22:30:39 +0500

    Updated

--- src/Shops/OnlineStore.java
+++ src/Shops/OnlineStore.java
@@ -1,6 +1,28 @@
 package Shops;
 
+import com.company.Item;
+import java.util.HashMap;
 
 public class OnlineStore extends Store {
-    
+	
+	//web address
+	 private String address;
+	 
+	 public OnlineStore(String name, double register, HashMap<Item, Integer> inventory,String address){
+		 super(name,register,inventory);
+		 this.address=address; 
+	 }
+	 public OnlineStore(String name, double register,String address){
+		 super(name,register);
+		 this.address=address; 
+	 }
+	 
+	 public OnlineStore(){
+		 this(null,0,null);
+	 }
+	 
+	 public String getAddress() {
+	        return address;
+	    }
+	
 }
