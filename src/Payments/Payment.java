487fafe604c5d1305de7ee020270f7f5111a51c5
Abdul Samad <abdul.samad4068@gmail.com>
2020-03-28 22:31:34 +0500

    Updated

--- src/Payments/Payment.java
+++ src/Payments/Payment.java
@@ -1,36 +1,34 @@
 package Payments;
 
-import java.time.LocalDateTime;
 
-public class Payment {
+abstract public class Payment {
     private final String id;
-    private final LocalDateTime date;
     private double amount;
 
-    public Payment(String id, LocalDateTime date, double amount) {
+    public Payment(String id, double amount) {
         this.id = id;
-        this.date = date;
         this.amount = amount;
     }
-
+    
     public Payment() {
-        this(null, LocalDateTime.now(), 0);
+        this(null, 0);
     }
 
     public String getId() {
         return id;
     }
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
     public double getAmount() {
         return amount;
+    }
+    
+    public void setAmount(double amout) {
+        this.amount= amout;
     }
 
     public void addAmount(double amount) {
         this.amount += amount;
     }
+    
 
 }
