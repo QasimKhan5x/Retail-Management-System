f994b5f5c2572ef5d85a44ef1889f30f60a95022
Abdul Samad <abdul.samad4068@gmail.com>
2020-03-28 22:33:11 +0500

    Updated

--- src/Payments/Cash.java
+++ src/Payments/Cash.java
@@ -2,4 +2,17 @@ package Payments;
 
 
 public class Cash extends Payment {
+	
+	public Cash(String id, double amount) {
+        super(id,amount);
+    }
+	
+	public Cash(){
+		this(null,0);
+	}
+	
+	public String getPaymentDetails() {
+		return String.format("The Payment of cash: "+getAmount());
+		
+	}
 }
