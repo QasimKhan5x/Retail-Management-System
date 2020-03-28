2608086e42e9f35cd2fcb79bfd8ddc4faffdb55d
Abdul Samad <abdul.samad4068@gmail.com>
2020-03-28 22:30:04 +0500

    Updated

--- src/Shops/Outlet.java
+++ src/Shops/Outlet.java
@@ -1,4 +1,28 @@
 package Shops;
 
-public class Outlet {
-}
+import com.company.Item;
+import java.util.HashMap;
+
+public class Outlet extends Store {
+	 private String address;
+	 
+	 public Outlet(String name, double register, HashMap<Item, Integer> inventory,String address){
+		 super(name,register,inventory);
+		 this.address=address; 
+	 }
+	 
+	 public Outlet(String name, double register,String address){
+		 super(name,register);
+		 this.address=address; 
+	 }
+	 
+	 public Outlet(){
+		 this(null,0,null);
+	 }
+	 
+	 public String getAddress() {
+	        return address;
+	    }
+	 
+	
+}	
